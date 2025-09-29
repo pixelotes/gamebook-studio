cb58d539f6bc9e555adf614cc4eda10fe77a7215
pixelotes <donpastor@gmail.com>
2025-09-29 16:39:50 +0200

    feat: migrate to vite@7.1.7

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';  // ← Essential for Tailwind
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);;
+root.render(<App />);
